9cf6f8882844d04ee1562855fb5b0ddd35cc87f5
sk <sk@DESKTOP-3CUDANH>
2023-12-20 01:02:41 +0530

    intialCommit

---
+++ assign/src/assign/main.java
@@ -0,0 +1,20 @@
+package assign;
+public class main {
+    public static void main(String[] args) {
+        int[] arr = {5, 28, 12, 10, 3, 07};
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second largest element: " + secondLargest);
+    }
+}
+
